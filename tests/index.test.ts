3cb36dc064334472bf91317c11436231d042ef2d
gzordrai <gzordrai@users.noreply.github.com>
2024-06-03 04:50:14 +0200

    Add tests

---
+++ tests/index.test.ts
@@ -0,0 +1,32 @@
+import { ROFLParser } from "../src";
+
+describe("ROFLParser", () => {
+    test("should throw error if file does not exist", () => {
+        expect(() => new ROFLParser("./tests/assets/nonexistent.rofl")).toThrow(Error);
+    });
+
+    test("should check the file extension", () => {
+        expect(() => new ROFLParser("./tests/assets/random_file.txt")).toThrow(Error);
+    });
+
+    test("should correctly check signature", () => {
+        expect(() => new ROFLParser("./tests/assets/invalid_signature_file_test.rofl")).toThrow(Error);
+    });
+
+    test("should check metadata", () => {
+        expect(() => new ROFLParser("./tests/assets/invalid_metadata_file_test.rofl").parse()).toThrow(Error);
+    });
+
+    test("should find pattern in the file", () => {
+        const metadata = new ROFLParser("./tests/assets/valid_file_test.rofl").parse();
+
+        expect(metadata).toBeDefined();
+        expect(metadata).toHaveProperty("gameLength");
+        expect(metadata.gameLength).toBe(1258259)
+        expect(metadata).toHaveProperty("lastGameChunkId");
+        expect(metadata.lastGameChunkId).toBe(44)
+        expect(metadata).toHaveProperty("lastKeyFrameId");
+        expect(metadata.lastKeyFrameId).toBe(21)
+        expect(metadata).toHaveProperty("statsJson");
+    });
+});
