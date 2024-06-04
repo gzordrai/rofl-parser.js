1bd0eb4174575d0694cb5c51a8a1bf0f41987510
gzordrai <gzordrai@users.noreply.github.com>
2024-06-04 03:45:01 +0200

    Update Metadata class to use processed metadata types and parse statsJson in Metadata instead of in ROFLParser

--- tests/Metadata.test.ts
+++ tests/Metadata.test.ts
@@ -1,4 +1,4 @@
-import { Metadata, RawMetadata } from "../src";
+import { JSONStats, Metadata, RawMetadata } from "../src";
 
 describe("Metadata", () => {
     const rawMetadata: RawMetadata = {
@@ -8,26 +8,32 @@ describe("Metadata", () => {
         statsJson: "{}"
     };
 
+    let metadata: Metadata;
+
+    beforeAll(() => {
+        metadata = new Metadata(rawMetadata);
+    });
+
     test("should create a new instance of Metadata", () => {
-        const metadata: Metadata = new Metadata(rawMetadata);
+        const statsJson: JSONStats = JSON.parse(rawMetadata.statsJson);
 
         expect(metadata).toBeInstanceOf(Metadata);
         expect(metadata.gameLength).toBe(rawMetadata.gameLength);
         expect(metadata.lastGameChunkId).toBe(rawMetadata.lastGameChunkId);
         expect(metadata.lastKeyFrameId).toBe(rawMetadata.lastKeyFrameId);
-        expect(metadata.statsJson).toBe(rawMetadata.statsJson);
+        expect(metadata.statsJson).toStrictEqual(statsJson);
     });
 
     test("should correctly convert to string", () => {
-        const metadata: Metadata = new Metadata(rawMetadata);
-        const expectedString: string = JSON.stringify(rawMetadata);
+        const expectedObject = {...rawMetadata, statsJson: JSON.parse(rawMetadata.statsJson)};
+        const expectedString: string = JSON.stringify(expectedObject);
 
         expect(metadata.toString()).toBe(expectedString);
     });
 
     test("should correctly convert to buffer", () => {
-        const metadata: Metadata = new Metadata(rawMetadata);
-        const expectedBuffer: Buffer = Buffer.from(JSON.stringify(rawMetadata));
+        const expectedObject = {...rawMetadata, statsJson: JSON.parse(rawMetadata.statsJson)};
+         const expectedBuffer: Buffer = Buffer.from(JSON.stringify(expectedObject));
 
         expect(metadata.toBuffer()).toEqual(expectedBuffer);
     });
